f91aabf8adb42a44e7c82a47405681bd8c1ccd05
chrispysz <krzysztof.pysz99@gmail.com>
2023-05-15 22:15:55 +0200

    improvements

--- api/window_predict.py
+++ api/window_predict.py
@@ -3,43 +3,24 @@ import torch
 import logging
 import time
 
+model = BertForSequenceClassification.from_pretrained("./models/AmBERT")
+tokenizer = BertTokenizer.from_pretrained("./models/tokenizer/")
 
-def predict_window(seq):
-
-    start = time.process_time()
-
-    seq_cutoff = 39
-
-    threshold = 0.5
-
-    model = BertForSequenceClassification.from_pretrained(
-        "./models/AmBERT")
-    tokenizer = BertTokenizer.from_pretrained("./models/tokenizer/")
-
-    current_label = 0
+def predict_window(seq, model, tokenizer, seq_cutoff=39, threshold=0.5):
+    global model
+    global tokenizer
 
     predictions = []
-    sequences = []
-    spec_preds = []
     seq_dicts = []
 
-    seq_proper = ''
-    for aa in seq:
-        seq_proper += aa+' '
-
     if len(seq) > seq_cutoff:
-        splits = len(seq)-seq_cutoff
+        splits = len(seq) - seq_cutoff
         for i in range(splits):
             subseq = seq[i:seq_cutoff+i+1]
             inputs = tokenizer(" ".join(subseq), return_tensors="pt")
             outputs = model(**inputs)
             logits = outputs.logits
             prediction = torch.softmax(logits, axis=-1).detach().numpy()
-            if prediction[0][1] > threshold:
-                predictions.append('1')
-            else:
-
-                predictions.append('0')
 
             seq_dict = {
                 "startIndex": i,
@@ -47,9 +28,7 @@ def predict_window(seq):
                 "prediction": str(prediction[0][1])
             }
             seq_dicts.append(seq_dict)
+            logging.info(f"Processed {i+1}/{splits}")
 
-            logging.warn("Processed " + str(i+1)+"/"+str(splits))
-
-    elapsed = time.process_time() - start
-    logging.warn(msg="Prediction processed in "+str(elapsed)+" seconds")
-    return (seq_dicts)
+    logging.info("Prediction completed")
+    return seq_dicts
